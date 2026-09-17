2dbcc655337d44f07e4c52cdd0d53d87c779210d
Abdulfatai Olarewaju <olarewaju Abdulfatai>
2026-09-17 11:51:15 +0100

    Fix API root route again

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,12 +33,15 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Finora API is running",
-  });
-});
+app.get(
+  "/",
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    throw new BadRequestException("This is a test error");
+    res.status(HTTPSTATUS.OK).json({
+      message: "Hello Subcribe to the channel",
+    });
+  })
+);
 
 app.use(`${BASE_PATH}/auth`, authRoutes);
 app.use(`${BASE_PATH}/user`, passportAuthenticateJwt, userRoutes);
